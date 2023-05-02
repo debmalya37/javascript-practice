8cd938a2115657649fd45818008901830921e469
Debmalya <debmalyasen37@gmail.com>
2023-05-03 00:14:31 +0530

    added nap.js and practiced .map() method

---
+++ random-all/map.js
@@ -0,0 +1,10 @@
+// MAP method :
+// In JavaScript, the map() method is used to create a new array by applying a given function to each element of an existing array. The new array will have the same length as the original array, but with each element transformed according to the function that was passed in.
+
+// Here's an example of how the map() method works:
+
+const numbers = [1, 2, 3, 4, 5, 6, 7];
+
+const newNum = numbers.map((value) => value + "day");
+
+console.log(newNum);
