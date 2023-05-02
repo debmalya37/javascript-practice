7f6889ad462775667c8d132f2ec6916f3998505c
Debmalya <debmalyasen37@gmail.com>
2023-05-02 22:55:53 +0530

    updated sorting.js with descending order sorting code

--- algo/sorting.js
+++ algo/sorting.js
@@ -4,3 +4,8 @@ const unsorted = [1, -1, 2, 3, -4, 0, -8];
 const sorting1 = unsorted.sort((a, b) => a - b);
 
 console.log(sorting1);
+
+// sort the array in descending order
+
+const sorting2 = unsorted.sort((x, y) => y - x);
+console.log(sorting2);
