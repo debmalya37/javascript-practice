00dc8d4cb8c499a83e02e1945b39e45034157be0
Debmalya <debmalyasen37@gmail.com>
2023-05-02 23:30:29 +0530

    added random-all folder and practiced arrays and stuff bcz feeling like exploring

---
+++ random-all/p1.js
@@ -0,0 +1,17 @@
+// check if array or not
+
+const { Console } = require("console");
+
+let word = ["bag", "doll", "school", "heart"];
+
+console.log(Array.isArray(word));
+
+// function params
+
+const person = {
+  name: "deb",
+  id: 1,
+  country: "ind",
+};
+
+console.log(typeof person);
