4a1c1dabfef3be21df607b066fe466996f94828b
Debmalya <debmalyasen37@gmail.com>
2023-05-04 23:25:36 +0530

    made the function of ordering the in alphabetical order

--- exercises/function.js
+++ exercises/function.js
@@ -1,9 +1,19 @@
 // function exercise
 // Write a JavaScript function that returns a string that has letters in alphabetical order
 
-function alphastring(name) {
-  const real = name;
-  return real;
+function alpha_order(name) {
+  return name.split("").sort().join("");
 }
-const proms = alphastring("deb");
-console.log(typeof proms);
+console.log(alpha_order("dcba"));
+// methods used :
+// The split() method is used to split a String object into an array of strings by separating the string into substrings.
+// Code : console.log('32243'.split(""));
+// Output : ["3", "2", "2", "4", "3"]
+
+// The sort() method is used to sort the elements of an array in place and returns the array.
+// Code : console.log(["3", "2", "2", "4", "3"].sort());
+// Output : ["2", "2", "3", "3", "4"]
+
+// The join() method is used to join all elements of an array into a string.
+// Code : console.log(["2", "2", "3", "3", "4"].join(""));
+// Output : "22334"
