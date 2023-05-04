b879a06daa35cb314df4daaf2c579c21dfbfc813
Debmalya <debmalyasen37@gmail.com>
2023-05-04 23:00:24 +0530

    exercising the function problems

---
+++ exercises/function.js
@@ -0,0 +1,9 @@
+// function exercise
+// Write a JavaScript function that returns a string that has letters in alphabetical order
+
+function alphastring(name) {
+  const real = name;
+  return real;
+}
+const proms = alphastring("deb");
+console.log(typeof proms);
