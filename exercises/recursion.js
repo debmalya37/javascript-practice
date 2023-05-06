9ca133691bf07ae7c351b61a1bd1f91ce5886032
Debmalya <debmalyasen37@gmail.com>
2023-05-06 23:39:05 +0530

    #5 interger range getter function added

--- exercises/recursion.js
+++ exercises/recursion.js
@@ -20,3 +20,17 @@ const gcd = function (a, b) {
   return gcd(b, a % b);
 };
 console.log(gcd(2154, 458));
+
+// #3 Write a JavaScript program to get integers in the range (x, y) using recursion.
+
+var range = function (start_num, end_num) {
+  if (end_num - start_num === 2) {
+    return [start_num + 1];
+  } else {
+    var list = range(start_num, end_num - 1);
+    list.push(end_num - 1);
+    return list;
+  }
+};
+
+console.log(range(2, 9));
